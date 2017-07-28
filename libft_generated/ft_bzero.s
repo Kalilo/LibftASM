d8f031a66292d92d23c3c1b79683db63c616129e
Kalilo <kalilohansmann@gmail.com>
2017-07-28 11:42:01 +0200

    Commented to understand

--- libft_generated/ft_bzero.s
+++ libft_generated/ft_bzero.s
@@ -5,32 +5,32 @@
 _ft_bzero:                              ## @ft_bzero
 	.cfi_startproc
 ## BB#0:
-	pushq	%rbp
+	pushq	%rbp;save state
 Ltmp0:
-	.cfi_def_cfa_offset 16
+	.cfi_def_cfa_offset 16;reserve temp
 Ltmp1:
-	.cfi_offset %rbp, -16
+	.cfi_offset %rbp, -16; reserve k
 	movq	%rsp, %rbp
 Ltmp2:
 	.cfi_def_cfa_register %rbp
-	movq	%rdi, -8(%rbp)
-	movq	%rsi, -16(%rbp)
-	movq	$0, -24(%rbp)
-	movq	-8(%rbp), %rsi
-	movq	%rsi, -32(%rbp)
+	movq	%rdi, -8(%rbp);load s
+	movq	%rsi, -16(%rbp);load n
+	movq	$0, -24(%rbp); init k
+	movq	-8(%rbp), %rsi; init temp
+	movq	%rsi, -32(%rbp);
 LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
-	movq	-24(%rbp), %rax
-	cmpq	-16(%rbp), %rax
-	jae	LBB0_3
+	movq	-24(%rbp), %rax; k
+	cmpq	-16(%rbp), %rax; cmp n, k
+	jae	LBB0_3; break
 ## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
-	movq	-24(%rbp), %rax
-	movq	-32(%rbp), %rcx
-	movb	$0, (%rcx,%rax)
-	movq	-24(%rbp), %rax
-	addq	$1, %rax
-	movq	%rax, -24(%rbp)
-	jmp	LBB0_1
-LBB0_3:
+	movq	-24(%rbp), %rax; k
+	movq	-32(%rbp), %rcx; n
+	movb	$0, (%rcx,%rax); load 0 into n[k]
+	movq	-24(%rbp), %rax; load k
+	addq	$1, %rax; inc k
+	movq	%rax, -24(%rbp); set new k
+	jmp	LBB0_1; loop
+LBB0_3:;return
 	popq	%rbp
 	retq
 	.cfi_endproc
