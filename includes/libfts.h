f13257552ae3a596f28cd6964ec85e6360e1da4a
Kalilo <kalilohansmann@gmail.com>
2017-07-28 10:27:44 +0200

    Added basic header file

---
+++ includes/libfts.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libfts.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/28 09:17:15 by khansman          #+#    #+#             */
+/*   Updated: 2017/07/28 09:17:16 by khansman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/*
+** ----------\
+** Includes   |
+** ----------/
+*/
+
+# include <string.h>
+
+/*
+** ----------\
+** Prototypes |
+** ----------/
+*/
+
+/*
+** Part1
+*/
+void	ft_bzero(void *s, size_t n);
+char	*ft_strcat(char *s1, const char *s2);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_puts(const char *s);
+
+/*
+** Part2
+*/
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strdup(const char *s1);
+
+/*
+** Part3
+*/
+void	ft_cat(int fd);
+
+/*
+**                                /----------\                                **
+** ----- ----- ----- ----- ----- |  The End   | ----- ----- ----- ----- ----- **
+**                                \----------/                                **
+*/
+#endif
