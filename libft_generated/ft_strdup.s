e969798c09d44cee5748af053ee74828555ac1e6
Kalilo <kalilohansmann@gmail.com>
2017-07-29 16:47:02 +0200

    Commented

--- libft_generated/ft_strdup.s
+++ libft_generated/ft_strdup.s
@@ -10,44 +10,44 @@ Ltmp0:
 	.cfi_def_cfa_offset 16
 Ltmp1:
 	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp; {
 Ltmp2:
 	.cfi_def_cfa_register %rbp
-	subq	$32, %rsp
-	movq	%rdi, -16(%rbp)
-	movq	$0, -24(%rbp)
+	subq	$32, %rsp; reserve 2 var
+	movq	%rdi, -16(%rbp); set s1
+	movq	$0, -24(%rbp); k = 0
 LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
-	movq	-24(%rbp), %rax
-	movq	-16(%rbp), %rcx
-	movsbl	(%rcx,%rax), %edx
-	cmpl	$0, %edx
+	movq	-24(%rbp), %rax; load k
+	movq	-16(%rbp), %rcx; load s1
+	movsbl	(%rcx,%rax), %edx; load s1[k]
+	cmpl	$0, %edx; break if (s1[k] == 0)
 	je	LBB0_3
 ## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
-	movq	-24(%rbp), %rax
-	addq	$1, %rax
-	movq	%rax, -24(%rbp)
-	jmp	LBB0_1
+	movq	-24(%rbp), %rax; load k
+	addq	$1, %rax; inc k
+	movq	%rax, -24(%rbp); save k
+	jmp	LBB0_1; loop
 LBB0_3:
-	movq	-24(%rbp), %rax
-	addq	$1, %rax
-	movq	%rax, %rdi
-	callq	_malloc
-	movq	%rax, -32(%rbp)
-	cmpq	$0, %rax
-	jne	LBB0_5
+	movq	-24(%rbp), %rax; load k
+	addq	$1, %rax; inc k
+	movq	%rax, %rdi; save k
+	callq	_malloc; malloc
+	movq	%rax, -32(%rbp); save s
+	cmpq	$0, %rax; if (s == NULL)
+	jne	LBB0_5; return
 ## BB#4:
-	movq	$0, -8(%rbp)
+	movq	$0, -8(%rbp); init copy
 	jmp	LBB0_9
 LBB0_5:
 	jmp	LBB0_6
 LBB0_6:                                 ## =>This Inner Loop Header: Depth=1
-	cmpq	$0, -24(%rbp)
+	cmpq	$0, -24(%rbp); cmp k, 0
 	jbe	LBB0_8
 ## BB#7:                                ##   in Loop: Header=BB0_6 Depth=1
-	movq	-24(%rbp), %rax
-	movq	-16(%rbp), %rcx
-	movb	(%rcx,%rax), %dl
-	movq	-24(%rbp), %rax
+	movq	-24(%rbp), %rax; load k
+	movq	-16(%rbp), %rcx; load s1
+	movb	(%rcx,%rax), %dl; load s1[k]
+	movq	-24(%rbp), %rax; load k
 	movq	-32(%rbp), %rcx
 	movb	%dl, (%rcx,%rax)
 	movq	-24(%rbp), %rax
@@ -64,7 +64,7 @@ LBB0_8:
 LBB0_9:
 	movq	-8(%rbp), %rax
 	addq	$32, %rsp
-	popq	%rbp
+	popq	%rbp; }
 	retq
 	.cfi_endproc
 
