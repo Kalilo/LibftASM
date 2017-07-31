0f0e51600a74571eb068219fd498be60bb6a9f30
gabrielpora <gabriel.groener@gmail.com>
2017-07-31 14:08:10 +0200

    please test my ft_strcat2.s file as I did not was to over write Kailio ft_strcat.s file

---
+++ srcs/ft_strcat2.s
@@ -0,0 +1,41 @@
+; **************************************************************************** ;
+;                                                                              ;
+;                                                         :::      ::::::::    ;
+;    ft_strcat.s                                        :+:      :+:    :+:    ;
+;                                                     +:+ +:+         +:+      ;
+;    By: khansman <marvin@42.fr>                    +#+  +:+       +#+         ;
+;                                                 +#+#+#+#+#+   +#+            ;
+;    Created: 2017/07/28 08:56:28 by khansman          #+#    #+#              ;
+;    Updated: 2017/07/28 08:56:29 by khansman         ###   ########.fr        ;
+;                                                                              ;
+; **************************************************************************** ;
+
+global _ft_strcat
+
+section .text
+
+;char	*ft_strcat(char *s1, const char *s2)
+
+_ft_strcat:
+	mov r11, rdi
+
+pass_rdi:
+	cmp byte[rdi], 0
+	je appending
+	inc rdi
+	jmp pass_rdi
+
+appending:
+	cmp byte[rsi], 0
+	je ending_programme
+	mov al, byte[rsi]
+	mov byte[rdi], al
+	inc rdi
+	inc rsi
+	jmp appending
+
+ending_programme
+	mov byte[rdi], 0
+	mov rax, r11
+	ret
+	
